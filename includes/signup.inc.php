5057fad220d87ec65c1cb027ee091b84a18e72e0
Tashi Yangchen <tyangchen005@gmail.com>
2025-01-22 21:09:02 +0600

    Handle user sign up

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -1,21 +1,51 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     // print_r($_POST);
+    // making access to dbh.inc.php which then connects to the database
+    include 'dbh.inc.php';
 
     $username = $_POST['username'];
     $pwd = $_POST['pwd'];
     $email = $_POST['email'];
 
-    // echo "<br>$username, $pwd, $email";
+    try{
+        // ensures that the file dbh.inc.php is included in the script only once.
+        // If the file is included more than once, PHP will raise a fatal error.
+        require_once "dbh.inc.php";
 
-    if (empty($username) || empty($pwd) || empty($email)){
-        echo "All fields are required!!!";
-    } else {
-        echo "<h1>Form well received</h1>";
-    };
+        // SQL query to be run in the database
+        $query = "INSERT INTO users (username, pwd, email) VALUES (:username, :pwd, :email);";
 
-} else {
-    echo "Invalid request";
+        // Prepares the query for execution, protecting against SQL injection. this is basically a security measure.
+        $statement = $pdo->prepare($query);
+
+        // Associates the placeholders(:username, :pwd, :email) in the query statement with actual values.
+        $statement->bindParam(":username", $username);
+        $statement->bindParam(":email", $email);
+        $statement->bindParam(":pwd", $pwd);
+        // INSERT INTO users (username, pwd, email) VALUES ('sonam', 's001', 'soom@gmail.com');
+        // Executes the prepared statement, inserting the data into the database.
+        $statement->execute();
+
+        // Closes the connection to the database.
+        $pdo = null;
+        $statement = null;
+
+        // Redirects to success.php file with a creation=success query string, indicating the operation was successful.
+        // header("Location: ../success.php?creation=success");
+        header("Location: ../success.php?creation=success");
+
+        // Ensures no further script execution after the redirect.
+        //  Terminates the script or closes this file.
+        die();
+    }
+    catch (PDOException $e){
+        // Catches and handles database-related exceptions displaying the error message if the query fails.
+        die ("Query failed: ". $e->getMessage());
+    }
+}else {
+    // If the request method is not POST, the user is redirected to the homepage.
+    header("Location: ../index.php");
 }
 
 
---
+++ success.php
@@ -0,0 +1,10 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>success</title>
+</head>
+<body>
+    <h1>Successfully signed up new user!</h1>
+</body>
+</html>
